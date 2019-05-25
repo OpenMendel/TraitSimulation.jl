3b0cf3156c70b6684c6858c3dbb23545cdc18c9e
Sarah <smji@g.ucla.edu>
2019-05-24 22:07:15 -0700

    modified tutorial

--- src/TraitSimulation.jl
+++ src/TraitSimulation.jl
@@ -10,13 +10,12 @@ include("calculate_mean_vector.jl")
 include("apply_inverse_link_new.jl")
 export LogLink, IdentityLink, SqrtLink, ProbitLink, LogitLink, InverseLink, CauchitLink, CloglogLink
 
-
 include("simulate_glm_trait_new.jl")
 export PoissonResponse, NormalResponse, BinomialResponse, BernoulliResponse, GammaResponse, InverseGaussianResponse, TResponse, WeibullResponse #Exporting these from the Distributions package 
 
 
 #this is the main functionality of this package, to run the actual simulation now for the split up responsedist type and linkfunction type
-function actual_simulation(mu, dist::ResponseDistribution, link::InverseLinkFunction) 
+function GLM_trait_simulation(mu, dist::ResponseDistribution, link::InverseLinkFunction) 
   transmu = apply_inverse_link(mu, link)
   Simulated_Trait = simulate_glm_trait(transmu, dist)
   return(Simulated_Trait)
@@ -35,7 +34,7 @@ simulate(trait::GLMTrait)
 this for GLM trait
 """
 function simulate(trait::GLMTrait)
-    simulated_trait = actual_simulation(trait.mu, trait.dist, trait.link)
+    simulated_trait = GLM_trait_simulation(trait.mu, trait.dist, trait.link)
     rep_simulation = DataFrame(trait1 = simulated_trait)
     return(rep_simulation)
 end
@@ -68,7 +67,7 @@ function simulate(trait::LMMTrait, n_reps::Int64)
 end
 
 
-export ResponseType, actual_simulation, mean_formula, VarianceComponent, append_terms!
+export ResponseType, GLM_trait_simulation, mean_formula, VarianceComponent, append_terms!
 export GLMTrait, Multiple_GLMTraits, LMMTrait, simulate, @vc, vcobjtuple
 export TResponse, WeibullResponse, PoissonResponse, NormalResponse, BernoulliResponse, BinomialResponse
 export GammaResponse, InverseGaussianResponse, ExponentialResponse
