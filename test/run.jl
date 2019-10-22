35b8c9f1bfa6dfbd5bd2e08e0df23d09d0fa05f9
Sarah <smji@g.ucla.edu>
2019-10-22 14:41:43 -0700

    testing for inverse links

---
+++ test/run.jl
@@ -0,0 +1,84 @@
+float64_input = Float64[0, 1]
+float64_output = Float64[0.5, 0.75]
+
+for i in eachindex(float64_input)
+  return(@test cauchit_inverse_link(float64_input[i]) == float64_output[i])
+end
+
+float32_input = Float32[0, 1]
+float32_output = Float32[0.5, 0.75]
+@test cauchit_inverse_link(float32_input[1]) isa Float32
+
+#And update our algorithm: 
+cauchit_inverse_link(x) = atan(x) / pi + one(x) / 2
+
+for i in eachindex(float32_input)
+  return(@test cauchit_inverse_link(float32_input[i]) == float32_output[i])
+end
+
+
+# cloglog
+float64_input = Float64[0, 1]
+float64_output = Float64[0.6321205588285577, 0.9340119641546875]
+
+for i in eachindex(float64_input)
+  return(@test cloglog_inverse_link(float64_input[i]) == float64_output[i])
+end
+
+float32_input = Float32[0, 1]
+float32_output = Float32[0.6321205588285577, 0.9340119641546875]
+@test cloglog_inverse_link(float32_input[1]) isa Float32
+
+#And update our algorithm: 
+function cloglog_inverse_link(x)
+  return one(x) - exp(-exp(x))
+end 
+
+for i in eachindex(float32_input)
+  return(@test cloglog_inverse_link(float32_input[i]) == float32_output[i])
+end
+
+# identity
+float64_input = Float64[0, 1]
+float64_output = Float64[0.0, 1.0]
+
+for i in eachindex(float64_input)
+  return(@test identity_inverse_link(float64_input[i]) == float64_output[i])
+end
+
+float32_input = Float32[0, 1]
+float32_output = Float32[0.0, 1.0]
+@test identity_inverse_link(float32_input[1]) isa Float32
+
+#And update our algorithm: 
+function identity_inverse_link(x)
+  return x
+end
+
+for i in eachindex(float32_input)
+  return(@test identity_inverse_link(float32_input[i]) == float32_output[i])
+end
+
+# inverse inverse 
+float64_input = Float64[1, 2]
+float64_output = Float64[1.0, 2.0]
+
+for i in eachindex(float64_input)
+  return(@test inverse_inverse_link(float64_input[i]) == float64_output[i])
+end
+
+float32_input = Float32[1, 2]
+float32_output = Float32[1.0, 2.0]
+@test inverse_inverse_link(float32_input[1]) isa Float32
+
+#And update our algorithm: 
+function inverse_inverse_link(x)
+  return one(x) / x
+end
+
+for i in eachindex(float32_input)
+  return(@test inverse_inverse_link(float32_input[i]) == float32_output[i])
+end
+
+
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,9 @@
-# this is a place for future testing
-print("hello world")
+using TraitSimulation
+using GLM # this already defines some useful distribution and link types
+using DataFrames # so we can test it 
+using StatsModels # useful distributions #lots more useful distributions
+using LinearAlgebra
+using Random
+using Test 
+
+include("run.jl")
