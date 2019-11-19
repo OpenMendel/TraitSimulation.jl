4b27fc8371b613fa889573ffbb2ad2f44587b7d1
Sarah <smji@g.ucla.edu>
2019-11-19 15:30:04 -0800

    update effect sizes

--- src/Simulate_RandomGLM.jl
+++ src/Simulate_RandomGLM.jl
@@ -9,40 +9,40 @@ The effect sizes are rounded to the second digit, throughout this example.
 Notice there is a random +1 or -1, so that there are effects that both increase and decrease the simulated trait value.
 """
 function Generate_Random_Model_Chisq(filepath::String, k::Int64; effectsizes = [], min_success_rate_per_row = 0.98, min_success_rate_per_col = 0.98, min_maf = 0.01, min_hwe_pval = 0.0, maxiters = 5)
-snpdata = SnpArray(filepath * ".bed")
+	snpdata = SnpArray(filepath * ".bed")
 
-rowmask, colmask =  SnpArrays.filter(snpdata,
-										min_success_rate_per_row = min_success_rate_per_row,
-										min_success_rate_per_col = min_success_rate_per_col,
-										min_maf = min_maf, min_hwe_pval = min_hwe_pval,
-										maxiters = maxiters)
+	rowmask, colmask =  SnpArrays.filter(snpdata,
+											min_success_rate_per_row = min_success_rate_per_row,
+											min_success_rate_per_col = min_success_rate_per_col,
+											min_maf = min_maf, min_hwe_pval = min_hwe_pval,
+											maxiters = maxiters)
 
-x = SnpArrays.filter(filepath, rowmask, colmask)
+	x = SnpArrays.filter(filepath, rowmask, colmask)
 
-SNP_data = SnpData(filepath)
-snpid = SNP_data.snp_info[!, :snpid]
-#k_indices = rand(1:length(snpid), k)
-k_indices = 1:k
-snpid_k = snpid[k_indices]
+	SNP_data = SnpData(filepath)
+	snpid = SNP_data.snp_info[!, :snpid]
+	#k_indices = rand(1:length(snpid), k)
+	k_indices = 1:k
+	snpid_k = snpid[k_indices]
 
-genotype_converted = DataFrame(convert(Matrix{Float64}, @view(x[:, k_indices])));
-names!(genotype_converted, Symbol.(snpid_k))
+		genotype_converted = DataFrame(convert(Matrix{Float64}, @view(x[:, k_indices])));
+		names!(genotype_converted, Symbol.(snpid_k))
 
-Simulated_ES = ones(k)
-if isempty(effectsizes)
-	# Generating Effect Sizes from theoretical Chisquared(df = 1) density, where the lower the minor allele frequency the larger the effect size
-	maf_k_snps = maf(x)[k_indices]
-	for i in 1:k
-		Simulated_ES[i] = rand([-1, 1]) .* (0.1 / sqrt.(maf_k_snps[i] .* (1 - maf_k_snps[i])))
+	Simulated_ES = ones(k)
+	if isempty(effectsizes)
+		# Generating Effect Sizes from theoretical Chisquared(df = 1) density, where the lower the minor allele frequency the larger the effect size
+		maf_k_snps = maf(x)[k_indices]
+		for i in 1:k
+			Simulated_ES[i] = rand([-1, 1]) .* (0.1 / sqrt.(maf_k_snps[i] .* (1 - maf_k_snps[i])))
+		end
+	else
+		copyto!(Simulated_ES, effectsizes)
 	end
-else
-	copyto!(Simulated_ES, effectsizes)
-end
 
 
-meanformula_k = FixedEffectTerms(Simulated_ES, snpid_k)
+	meanformula_k = FixedEffectTerms(Simulated_ES, snpid_k)
 
-return(meanformula_k, genotype_converted)
+	return(meanformula_k, Simulated_ES, genotype_converted)
 end
 
 # simulation
