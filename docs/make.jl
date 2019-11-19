597cf9cc9d99a25ff737bc9433c37e0ca6d1ce1c
Sarah <smji@g.ucla.edu>
2019-11-18 21:16:09 -0800

    updating make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,19 @@
 using Documenter, TraitSimulation
 
 makedocs(
-  doctest  = false,
-  format   = Documenter.HTML(),
-  modules  = [TraitSimulation],
-  clean    = true,
-  sitename = "TraitSimulation.jl",
-  authors  = "Sarah Ji",
-  pages = [
-    "Home"       => "index.md",
-    "GLM"   => "man/GLM.md",
-    "LMM" => "man/LMM.md",
-    "Example"   => "man/RareVariantExample.md",
-  ]
+    format = Documenter.HTML(),
+    sitename = "TraitSimulation.jl",
+    authors = "Sarah Ji, Kenneth Lange, Janet Sinsheimer, Eric Sobel",
+    clean = true,
+    debug = true,
+    pages = [
+        "index.md"
+    ]
 )
 
 deploydocs(
-  repo   = "github.com/OpenMendel/TraitSimulation.jl.git",
-  target = "build",
-  deps   = nothing,
-  make   = nothing
+    repo   = "github.com/OpenMendel/TraitSimulation.jl.git",
+    target = "build",
+    deps   = nothing,
+    make   = nothing
 )
