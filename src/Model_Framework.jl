dc53b8f022b79acd8280609641992fe3297ef3b4
Sarah <smji@g.ucla.edu>
2019-11-21 17:42:55 -0800

    using GLM package

--- src/Model_Framework.jl
+++ src/Model_Framework.jl
@@ -1,33 +1,60 @@
 """
 GLMTrait
-GLMTrait object is one of the two model framework objects. Stores information about the simulation of a single trait, under the Generalized Linear Model Framework.
+GLMTrait object is one of the two model framework objects.
+GLMTrait stores information about the simulation of a single trait, under the Generalized Linear Model Framework.
+GLM.formula store the evaluated formula
+GLM.mu stores the untransformed mean
+GLM.link stores the link function, by default canonical link. For Gamma and the NegativeBinomial responde distributions, we use the LogLink() function by default.
 """
-struct GLMTrait{D<:ResponseDistribution, L<:InverseLinkFunction}
+struct GLMTrait{D, L<:GLM.Link}
   formula::String
   mu::Vector{Float64}
-  dist:: D
-  link:: L
+  dist::Type{D}
+  link::L
 end
 
-function GLMTrait(mu::Number, df, dist::D, link::L) where {D, L}
-    return(GLMTrait{D, L}(string(mu), repeat([mu], size(df, 1)), dist, link))
+#If distribution is Gamma or NegativeBinomial we use the LogLink by default
+function GLMTrait(mu::Number, df, dist::Type{NegativeBinomial}; link = GLM.LogLink())
+    return(GLMTrait(string(mu), repeat([mu], size(df, 1)), dist, link))
 end
-
-function GLMTrait(formula::String, df, dist::D, link::L) where {D, L}
+function GLMTrait(formula::String, df, dist::Type{NegativeBinomial}; link = GLM.LogLink())
     mu = mean_formula(formula, df)
-    return(GLMTrait{D, L}(formula, mu, dist, link))
+    return(GLMTrait(formula, mu, dist, link))
 end
 
-function Multiple_GLMTraits(formulas, df, dist::ResponseDistribution, link::InverseLinkFunction)
-  vec = [GLMTrait(formulas[i], df, dist, link) for i in 1:length(formulas)] #vector of GLMTrait objects
-  return(vec)
+
+function GLMTrait(mu::Number, df, dist::Type{Gamma}; link = GLM.LogLink())
+    return(GLMTrait(string(mu), repeat([mu], size(df, 1)), dist, link))
 end
+
+function GLMTrait(formula::String, df, dist::Type{Gamma}; link = GLM.LogLink())
+    mu = mean_formula(formula, df)
+    return(GLMTrait(formula, mu, dist, link))
+end
+
+
+## given evaluated mean vector
+function GLMTrait(mu::Number, df, dist::D; link = canonicallink(dist())) where D
+    return(GLMTrait(string(mu), repeat([mu], size(df, 1)), dist, link))
+end
+
+## given formula and dataframe for mean vector evaluation
+function GLMTrait(formula::String, df, dist::D; link = canonicallink(dist())) where {D, L}
+    mu = mean_formula(formula, df)
+    return(GLMTrait(formula, mu, dist, link))
+end
+
+
+# function Multiple_GLMTraits(formulas, df, dist::D; link = canonicallink(dist())) where D
+#   vec = [GLMTrait(formulas[i], df, dist, link) for i in 1:length(formulas)] #vector of GLMTrait objects
+#   return(vec)
+# end
 
 # we put type of the dist vector as Any since we want to allow for any ResponseType{Poisson(), LogLink()}, ResponseType{Normal(), IdentityLink()}
-function Multiple_GLMTraits(formulas::Vector{String}, df::DataFrame, dist::Vector, link::Vector)
-  vec = [GLMTrait(formulas[i], df, dist[i], link[i]) for i in 1:length(formulas)]
-  return(vec)
-end
+# function Multiple_GLMTraits(formulas::Vector{String}, df::DataFrame, dist::Vector; link = canonicallink.(dist[i]() for i in 1:length(dist)))
+#   vec = [GLMTrait(formulas[i], df, dist[i], link[i]) for i in 1:length(formulas)]
+#   return(vec)
+# end
 
 
 # lmm: multiple traits (MVN)
