dea3f65047208d0a02622fa564da89c2ae4fe485
Sarah <smji@g.ucla.edu>
2019-11-24 21:19:35 -0800

    updating testing later

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,17 @@
-using TraitSimulation
-using GLM # this already defines some useful distribution and link types
-using DataFrames # so we can test it 
-using StatsModels # useful distributions #lots more useful distributions
-using LinearAlgebra
-using Random
-using Test 
-using Statistics
+# using TraitSimulation
+# using GLM # this already defines some useful distribution and link types
+# using DataFrames # so we can test it 
+# using StatsModels # useful distributions #lots more useful distributions
+# using LinearAlgebra
+# using Random
+# using Test 
+# using Statistics
 
-@testset "inverselink" begin 
-	include("inverselinktest.jl")
-end
+# @testset "inverselink" begin 
+# 	include("inverselinktest.jl")
+# end
 
 
-@testset "simulatedistribution" begin 
-	include("distributiontest.jl")
-end
+# @testset "simulatedistribution" begin 
+# 	include("distributiontest.jl")
+# end
