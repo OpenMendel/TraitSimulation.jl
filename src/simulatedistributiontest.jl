07a386343f4f8bfd4d49b940827b50a0652a572c
Sarah <smji@g.ucla.edu>
2020-01-22 18:04:25 -0800

    get rid of unnecessary test file

--- src/simulatedistributiontest.jl
+++
@@ -1,6 +0,0 @@
-function simulate_glm_trait(μ, dist::ExponentialResponse)
-  if any(μ_i -> μ_i < 0, μ)
-      error("Rate cannot be negative for an Exponential Distribution!")
-    end
-  return exponential_deviate.(μ)
-end
