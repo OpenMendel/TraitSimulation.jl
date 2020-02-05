ec313014063e8c166fefe9b7b9ce9c2d19ebbf3e
Sarah <smji@g.ucla.edu>
2020-02-04 18:12:46 -0800

    update to use MultinomialModels package

--- src/Model_Framework.jl
+++ src/Model_Framework.jl
@@ -52,47 +52,6 @@ function buildresponsedist(dist::D, mu, transmu) where D
   return(responsedist)
 end
 
-"""
-    rpolr(x, β, θ)
-Generate a random integer `Y` such that `P(Y≤j) = g^{-1}(θ[j]-x'β)`.
-`θ` has to be monotone `θ[1] ≤ ... ≤ θ[J-1]`.
-"""
-function rpolr(
-    x::AbstractVector,
-    β::AbstractVector,
-    θ::AbstractVector,
-    link::GLM.Link
-    )
-    # number of categories
-    J = length(θ) + 1
-    # check monotonicity of θj
-    issorted(θ) || throw(ArgumentError("θ[j] should be nondecreasing."))
-    # generate category according to cumulative probability
-    iprod = dot(x, β)
-    q = rand()
-    for j in 1:J-1
-        ηj = θ[j] - iprod
-        cumprobj = GLM.linkinv(link, ηj)
-        q ≤ cumprobj && (return j)
-    end
-    return J
-end
-
-"""
-    rpolr(X, β, θ)
-Generate a vector of random integers `Y` such that
-`P(Y[i]≤j) = g^{-1}(θ[j]-X[:,j]'β)`.
-`θ` has to be monotone `θ[1] ≤ ... ≤ θ[J-1]`.
-"""
-function rpolr(
-    X::AbstractMatrix,
-    β::AbstractVector,
-    θ::AbstractVector,
-    link::GLM.Link
-    )
-    @views Y = [rpolr(X[i, :], β, θ, link) for i in 1:size(X, 1)]
-end
-
 #rpolr(X, β, θ, link)
 struct OrdinalTrait{T, L<:GLM.Link} <: AbstractTrait
   X::Matrix{T}
@@ -105,7 +64,6 @@ struct OrdinalTrait{T, L<:GLM.Link} <: AbstractTrait
 end
 
 # theta needs to be in increasing order
-#uni1 = OrdinalTrait(rand(10,2), rand(2), sort(rand(3)), LogitLink())
 
 # lmm: multiple traits
 """
@@ -116,23 +74,21 @@ struct LMMTrait{T} <: AbstractTrait
   formulas::Vector{String}
   mu::Matrix{Float64}
   vc::T
-  function LMMTrait(formulas, df, vc::T) where T
-    n_traits = length(formulas)
-    n_people = size(df)[1]
-    mu = zeros(n_people, n_traits)
-    for i in 1:n_traits
-      #calculate the mean vector
-      mu[:, i] += mean_formula(formulas[i], df)
-    end
-    return(new{T}(formulas, mu, vc))
-  end
-
   function LMMTrait(mu, vc::T) where T
-    return(new{T}(String[],mu, vc))
+    return(new{T}(String[], mu, vc))
   end
-
 end
 
+function LMMTrait(formulas, df, vc::T) where T
+  n_traits = length(formulas)
+  n_people = size(df)[1]
+  mu = zeros(n_people, n_traits)
+  for i in 1:n_traits
+    #calculate the mean vector
+    mu[:, i] += mean_formula(formulas[i], df)
+  end
+  return(new{T}(formulas, mu, vc))
+end
 # ## given evaluated mean matrix
 # function LMMTrait(mu::AbstractArray{T, 2}, vc::U) where {T, U}
 #     return(LMMTrait{U}(string.(mu), mu, vc))
